d83d19e47fa8e9f1d1ac1af7c12fe06cf5d07bf3
combrain817 <67549703+combrain817@users.noreply.github.com>
2021-02-02 00:18:50 +0900

    Header & Welcome 섹션

---
+++ App Official Landing Website/custom.js
@@ -0,0 +1,20 @@
+$(() => {
+    /*Slick Slider - Welcome*/
+    $('.slideshow').slick({
+        infinite: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        speed: 1000,
+        pauseOnHover: false
+    });
+
+    /*TypeIt -welcome*/
+    $('#typing').typeIt({
+        strings: ['Business strategy.', 'Innovation Plan.', 'Creative Idea.' ],
+        speed: 100,
+        autoStart: true,
+        breakLines: false,
+    })
+})
